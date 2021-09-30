8c51192fe6c54deb1add34f388ca2d2651af6dda
dishankgoel <dishankgoel1729@gmail.com>
2021-10-01 01:50:05 +0530

    Fix Autocompletion

--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -34,6 +34,7 @@ Plug 'nvim-telescope/telescope-fzy-native.nvim'
 
 " Install nvim-cmp
 Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
 
 " Install snippet engine (This example installs [hrsh7th/vim-vsnip](https://github.com/hrsh7th/vim-vsnip))
 Plug 'hrsh7th/vim-vsnip'
--- nvim/.config/nvim/lua/configs/cmp.lua
+++ nvim/.config/nvim/lua/configs/cmp.lua
@@ -1,86 +1,43 @@
-local compare = require('cmp.config.compare')
-local types = require('cmp.types')
+-- Setup nvim-cmp.
+  local cmp = require'cmp'
 
-local WIDE_HEIGHT = 40
-local cmp = require'cmp'
----@return cmp.ConfigSchema
-cmp.setup({
-    completion = {
-      autocomplete = {
-        types.cmp.TriggerEvent.TextChanged,
-      },
-      completeopt = 'menu,menuone,noselect',
-      keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)]],
-      keyword_length = 1,
-      get_trigger_characters = function(trigger_characters)
-        return trigger_characters
-      end,
-    },
-
+  cmp.setup({
     snippet = {
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-        end,
-    },
+      expand = function(args)
+        -- For `vsnip` user.
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` user.
 
+        -- For `luasnip` user.
+        -- require('luasnip').lsp_expand(args.body)
 
-    preselect = types.cmp.PreselectMode.Item,
-
-    documentation = {
-      border = { '', '', '', ' ', '', '', '', ' ' },
-      winhighlight = 'NormalFloat:CmpDocumentation,FloatBorder:CmpDocumentationBorder',
-      maxwidth = math.floor((WIDE_HEIGHT * 2) * (vim.o.columns / (WIDE_HEIGHT * 2 * 16 / 9))),
-      maxheight = math.floor(WIDE_HEIGHT * (WIDE_HEIGHT / vim.o.lines)),
-    },
-
-    confirmation = {
-      default_behavior = types.cmp.ConfirmBehavior.Insert,
-      get_commit_characters = function(commit_characters)
-        return commit_characters
+        -- For `ultisnips` user.
+        -- vim.fn["UltiSnips#Anon"](args.body)
       end,
     },
-
-    sorting = {
-      priority_weight = 2,
-      comparators = {
-        compare.offset,
-        compare.exact,
-        compare.score,
-        compare.kind,
-        compare.sort_text,
-        compare.length,
-        compare.order,
-      },
-    },
-
-    event = {},
-
     mapping = {
-        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        })
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
-
-    formatting = {
-      deprecated = true,
-      format = function(_, vim_item)
-        return vim_item
-      end,
-    },
-
-    experimental = {
-      ghost_text = false,
-    },
-
     sources = {
-        { name = 'nvim_lua' },
-        { name = 'buffer' },
+      { name = 'nvim_lsp' },
 
-    },
+      -- For vsnip user.
+      { name = 'vsnip' },
+
+      -- For luasnip user.
+      -- { name = 'luasnip' },
+
+      -- For ultisnips user.
+      -- { name = 'ultisnips' },
+
+      { name = 'buffer' },
+    }
   })
+
+  -- Setup lspconfig.
+  require('lspconfig')['clangd'].setup {
+    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  }
