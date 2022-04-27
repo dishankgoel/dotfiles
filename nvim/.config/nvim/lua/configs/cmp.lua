b73613376b386e5742c3e90aef06fb1e23c4519e
dishankgoel <dishankgoel1729@gmail.com>
2022-04-27 16:59:52 +0530

    update

--- nvim/.config/nvim/lua/configs/cmp.lua
+++ nvim/.config/nvim/lua/configs/cmp.lua
@@ -13,6 +13,13 @@ require('lspkind').init({
     with_text = true,
 })
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
+
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -32,6 +39,28 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
     },
     formatting = {
         format = function(entry, vim_item)
