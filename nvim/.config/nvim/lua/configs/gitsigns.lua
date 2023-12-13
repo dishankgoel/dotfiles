d9a07b8dc6a2e803dac323ea1ebe1c3d707c63d7
Dishank <dishank@Dishanks-MacBook-Pro.local>
2023-12-13 18:35:53 +0530

    chore: gitsigns sync

--- nvim/.config/nvim/lua/configs/gitsigns.lua
+++ nvim/.config/nvim/lua/configs/gitsigns.lua
@@ -1,10 +1,11 @@
 require('gitsigns').setup {
   signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add          = { text = '│' },
+    change       = { text = '│' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
   },
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
   numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -21,6 +22,7 @@ require('gitsigns').setup {
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
     delay = 0,
     ignore_whitespace = false,
+    virt_text_priority = 100,
   },
   current_line_blame_formatter = '<author>, <author_time:%d-%m-%Y> - <summary>',
   sign_priority = 6,
